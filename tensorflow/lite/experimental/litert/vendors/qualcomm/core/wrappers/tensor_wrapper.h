4745f2779f71e058eab7261158fd178a9d5c6929
chunhsue <quic_chunhsue@quicinc.com>
2025-02-21 21:18:47 +0800

    wrapper tests

--- tensorflow/lite/experimental/litert/vendors/qualcomm/core/wrappers/tensor_wrapper.h
+++ tensorflow/lite/experimental/litert/vendors/qualcomm/core/wrappers/tensor_wrapper.h
@@ -1,16 +1,51 @@
-//  Copyright (c) Qualcomm Innovation Center, Inc.
-//  All Rights Reserved.
+// Copyright (c) Qualcomm Innovation Center, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 #ifndef TENSORFLOW_LITE_EXPERIMENTAL_LITERT_VENDORS_QUALCOMM_CORE_WRAPPERS_TENSOR_WRAPPER_H_
 #define TENSORFLOW_LITE_EXPERIMENTAL_LITERT_VENDORS_QUALCOMM_CORE_WRAPPERS_TENSOR_WRAPPER_H_
 
+#include <iostream>
+#include <optional>
 #include <string>
 #include <vector>
 
-#include "third_party/qairt/latest/include/QNN/QnnTypes.h"
+#include "absl/types/span.h"
 #include "tensorflow/lite/experimental/litert/vendors/qualcomm/core/wrappers/quantize_params_wrapper.h"
+#include "third_party/qairt/latest/include/QNN/QnnTypes.h"
 
 namespace qnn {
+
+// Get the Qnn_DataType_t associated with given C++ type.
+template <typename T>
+inline constexpr Qnn_DataType_t GetQnnDataType(const bool is_quant) {
+  if constexpr (std::is_same_v<T, bool>) {
+    return QNN_DATATYPE_BOOL_8;
+  } else if constexpr (std::is_same_v<T, std::uint8_t>) {
+    return is_quant ? QNN_DATATYPE_UFIXED_POINT_8 : QNN_DATATYPE_UINT_8;
+
+  } else if constexpr (std::is_same_v<T, std::int8_t>) {
+    return is_quant ? QNN_DATATYPE_SFIXED_POINT_8 : QNN_DATATYPE_INT_8;
+
+  } else if constexpr (std::is_same_v<T, std::uint16_t>) {
+    return is_quant ? QNN_DATATYPE_UFIXED_POINT_16 : QNN_DATATYPE_UINT_16;
+
+  } else if constexpr (std::is_same_v<T, std::int16_t>) {
+    return is_quant ? QNN_DATATYPE_SFIXED_POINT_16 : QNN_DATATYPE_INT_16;
+
+  } else if constexpr (std::is_same_v<T, std::uint32_t>) {
+    return is_quant ? QNN_DATATYPE_UFIXED_POINT_32 : QNN_DATATYPE_UINT_32;
+
+  } else if constexpr (std::is_same_v<T, std::int32_t>) {
+    return is_quant ? QNN_DATATYPE_SFIXED_POINT_32 : QNN_DATATYPE_INT_32;
+
+  } else if constexpr (std::is_same_v<T, float>) {
+    return QNN_DATATYPE_FLOAT_32;
+
+  } else {
+    static_assert(false, "Uknown C++ type");
+  }
+  return QNN_DATATYPE_UNDEFINED;
+}
 
 std::size_t GetDataTypeSize(const Qnn_DataType_t data_type);
 
@@ -51,6 +86,11 @@ class TensorWrapper final {
     return quantize_params_;
   };
 
+  const bool IsQuant() const {
+    return !std::holds_alternative<UndefinedQuantizeParamsWrapper>(
+        quantize_params_);
+  };
+
   bool IsPerTensorQuantWithOffsetDiff(const TensorWrapper& rhs) const;
 
   bool IsQuant8() const {
@@ -81,8 +121,6 @@ class TensorWrapper final {
 
   void SetTensorData(std::uint32_t bytes, const void* data);
 
-  const std::vector<std::byte>& GetTensorData() const { return owned_data_; }
-
   // Allocate memory on owned_data_ for output tensors
   void AllocateOutputTensorBuffer() {
     owned_data_.resize(GetTensorSize());
@@ -93,6 +131,9 @@ class TensorWrapper final {
   const void* GetStaticTensorData() const {
     return qnn_tensor_.v2.clientBuf.data;
   };
+
+  template <typename T>
+  std::optional<absl::Span<const T>> GetStaticTensorData() const;
 
  private:
   size_t GetTensorSize() const;
@@ -109,6 +150,35 @@ class TensorWrapper final {
 
 using TensorWrapperRef = std::reference_wrapper<TensorWrapper>;
 
+template <typename T>
+std::optional<absl::Span<const T>> TensorWrapper::GetStaticTensorData() const {
+  if (GetDataType() != GetQnnDataType<T>(IsQuant())) {
+    std::cout << "ERROR: GetStaticTensorData() with incorrect template type."
+              << std::endl;
+    return std::nullopt;
+  }
+
+  if (qnn_tensor_.v2.clientBuf.dataSize == 0 ||
+      qnn_tensor_.v2.clientBuf.data == nullptr) {
+    std::cout << "ERROR: Empty StaticTensorData." << std::endl;
+    return std::nullopt;
+  }
+
+  if (qnn_tensor_.v2.clientBuf.dataSize != GetTensorSize()) {
+    std::cout << "ERROR: Tensor size inconsistent with stored data."
+              << std::endl;
+    return std::nullopt;
+  }
+
+  uint32_t num_elements = qnn_tensor_.v2.clientBuf.dataSize / sizeof(T);
+  if (!num_elements) {
+    std::cout << "ERROR: No element in this tensor." << std::endl;
+    return std::nullopt;
+  }
+
+  return absl::MakeConstSpan(
+      reinterpret_cast<const T*>(qnn_tensor_.v2.clientBuf.data), num_elements);
+}
 }  // namespace qnn
 
 #endif  // TENSORFLOW_LITE_EXPERIMENTAL_LITERT_VENDORS_QUALCOMM_CORE_WRAPPERS_TENSOR_WRAPPER_H_
