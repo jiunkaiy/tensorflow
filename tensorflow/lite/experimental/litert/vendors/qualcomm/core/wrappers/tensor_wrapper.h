38e32b2338339a914ddf0a317fca79e02d6e1e11
chunhsue <quic_chunhsue@quicinc.com>
2025-03-04 17:52:23 +0800

    Qualcomm AI Engine Direct - Compile QINT16 as QUINT16

--- tensorflow/lite/experimental/litert/vendors/qualcomm/core/wrappers/tensor_wrapper.h
+++ tensorflow/lite/experimental/litert/vendors/qualcomm/core/wrappers/tensor_wrapper.h
@@ -15,6 +15,7 @@
 #include "third_party/qairt/latest/include/QNN/QnnTypes.h"
 
 namespace qnn {
+constexpr uint32_t kInt16ToUint16 = 32768;
 
 // Get the Qnn_DataType_t associated with given C++ type.
 template <typename T>
@@ -82,10 +83,20 @@ class TensorWrapper final {
     return quantize_params_;
   };
 
-  const bool IsQuant() const {
+  bool IsQuant() const {
     return !std::holds_alternative<UndefinedQuantizeParamsWrapper>(
         quantize_params_);
   };
+
+  bool IsPerTensorQuant() {
+    return std::holds_alternative<ScaleOffsetQuantizeParamsWrapper>(
+        quantize_params_);
+  }
+
+  bool IsPerChannelQuant() {
+    return std::holds_alternative<AxisScaleOffsetQuantizeParamsWrapper>(
+        quantize_params_);
+  }
 
   bool IsPerTensorQuantWithOffsetDiff(const TensorWrapper& rhs) const;
 
@@ -100,8 +111,6 @@ class TensorWrapper final {
   }
 
   Qnn_DataType_t GetDataType() const;
-
-  void SetDataType(Qnn_DataType_t data_type);
 
   bool IsSubgraphInput() const {
     return GetTensorType() == QNN_TENSOR_TYPE_APP_WRITE;
@@ -231,10 +240,17 @@ class TensorWrapper final {
 
   size_t GetTensorBytes() const;
 
+  void ConvertQint16ToQuint16();
+
  private:
   Qnn_TensorType_t GetTensorType() const;
 
   void SetTensorDataViaVoidPtr(std::uint32_t bytes, const void* data);
+
+  bool HasStaticData() const {
+    return qnn_tensor_.v2.clientBuf.dataSize != 0 &&
+           qnn_tensor_.v2.clientBuf.data != nullptr;
+  }
 
   Qnn_Tensor_t qnn_tensor_{.version = QNN_TENSOR_VERSION_2,
                            .v2 = QNN_TENSOR_V2_INIT};
@@ -260,9 +276,8 @@ std::optional<absl::Span<const T>> TensorWrapper::GetStaticTensorData() const {
     return std::nullopt;
   }
 
-  if (qnn_tensor_.v2.clientBuf.dataSize == 0 ||
-      qnn_tensor_.v2.clientBuf.data == nullptr) {
-    QNN_LOG_ERROR("Empty StaticTensorData.");
+  if (!HasStaticData()) {
+    QNN_LOG_ERROR("Empty static tensor data.");
     return std::nullopt;
   }
 
